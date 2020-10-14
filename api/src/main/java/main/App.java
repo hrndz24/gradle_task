c2f61f0e44e4bfdfce27312f843dbd314fa1f2b6
Natalie <natalli.buyanova.01@mail.ru>
2020-10-15 01:36:54 +0300

    Initial commit

---
+++ api/src/main/java/main/App.java
@@ -0,0 +1,10 @@
+package main;
+
+import utils.Utils;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+    }
+}
---
+++ core/src/main/java/utils/Utils.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Utils {
+
+    public static boolean isAllPositiveNumbers(String... str) {
+        for (String s : str) {
+            if (!StringUtils.isPositiveNumber(s)) return false;
+        }
+        return true;
+    }
+}
